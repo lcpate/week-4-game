7b9ad3e87b160001bd140c6708b7015b0d7046a9
Claire Pate <clairepate@Claires-Air.attlocal.net>
2017-10-15 18:31:37 -0400

    started java

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+
+var targetNum ="";
+var crystalValue="";
+var score = 0;
+var wins = 0;
+var losses = 0;
+
+$(document).ready(function(){
+
+
+	$("#pink").on("click", function(){
+	alert("you clicked the pink crystal!")
+	});
+
+	$("#blue").on("click", function(){
+	alert("you clicked the blue crystal!")
+	});
+
+	$("#yellow").on("click", function(){
+	alert("you clicked the yellow crystal!")
+	});
+
+	$("#purple").on("click", function(){
+	alert("you clicked the purple crystal!")
+	});
+
+});

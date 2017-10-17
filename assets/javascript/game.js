53e04782cb3ee966b654952b8b24ce26578f802c
Claire Pate <clairepate@Claires-MacBook-Air.local>
2017-10-17 18:29:17 -0400

    updated java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,87 @@
 
-var targetNum =50;
-var crystalValue="";
+var targetNum = "";
+var pinkValue="";
+var blueValue="";
+var yellowValue="";
+var purpleValue="";
 var score = 0;
 var wins = 0;
 var losses = 0;
 
+
 $(document).ready(function(){
 
+
+// Functions to randomize crystal value and target number
  $(".btn-success").on("click", function() {
-        var crystalValue = Math.floor(Math.random() * 11) + 1;
-        	$("#pink, #blue, #yellow, #purple").text(crystalValue);
-        	console.log(crystalValue);
+        var pinkValue = Math.floor(Math.random() * 11) + 1;
+        	$("#pink").text(pinkValue);
+        	console.log(pinkValue);;
+
+        var blueValue = Math.floor(Math.random() * 11) + 1;
+        	$("#blue").text(blueValue);
+        	console.log(blueValue);
+
+        var yellowValue = Math.floor(Math.random() * 11) + 1;
+        	$("#yellow").text(yellowValue);
+        	console.log(yellowValue);
+
+        var purpleValue = Math.floor(Math.random() * 11) + 1;
+        	$("#purple").text(purpleValue);
+        	console.log(purpleValue);
+
+        var targetNum = Math.floor(Math.random() * 101) + 19;
+        	$("#targetScore").text(targetNum);
+        	console.log("target score" + targetNum);
 
 
- 
+
+
+ // Functions to increase score/ wins/ losses when the crystal is selected
 	$("#pink").on("click", function(){
-		score += crystalValue;
-		alert("New Score" + score);
+		$("#userScore").text(score += pinkValue);
 		if (score === targetNum){
 			alert("You Win!");
-		} else if (score >= targetNum)
+		} else if (score >= targetNum) 
 			alert("You Lose!");
-		console.log(score);
+			losses++;
+		console.log("score" + score);
 
 	});
 
 	$("#blue").on("click", function(){
-		score += crystalValue;
-		alert("New Score" + score);
+		$("#userScore").text(score += blueValue);
 		if (score === targetNum){
 			alert("You Win!");
 		} else if (score >= targetNum)
 			alert("You Lose!");
+			losses++;
 		console.log(score);
 	});
 
 	$("#yellow").on("click", function(){
-		score += crystalValue;
-		alert("New Score" + score);
+		$("#userScore").text(score += yellowValue);
 		if (score === targetNum){
 			alert("You Win!");
 		} else if (score >= targetNum)
 			alert("You Lose!");
+			losses++;
 		console.log(score);
 	});
 
 	$("#purple").on("click", function(){
-		score += crystalValue;
-		alert("New Score" + score);
+		$("#userScore").text(score += purpleValue);
 		if (score === targetNum){
 			alert("You Win!");
 		} else if (score >= targetNum)
 			alert("You Lose!");
+			losses++;
 		console.log(score);
 	});
 
-	$("#targetScore").text(targetNum);
-
-	$("#userScore").text(score);
 
 
-
- // When random-button is clicked...
-//       $(".btn-success").on("click", function() {
-//         var crystalValue = Math.floor(Math.random() * 11) + 1;
-//         	$("#pink", "#blue", "#yellow", "#purple").text(crystalValue);
-
-//       // $(".btn-success").on("click", function() {
-//       //   var crystalValue = Math.floor(Math.random() * 11) + 1;
-//       //   	$("#blue").text(crystalValue);
-//       //   });
-
-//       // $(".btn-success").on("click", function() {
-//       //   var crystalValue = Math.floor(Math.random() * 11) + 1;
-//       //   	$("#yellow").text(crystalValue);
-//       //   });
-
-//       //  $(".btn-success").on("click", function() {
-//       //   var crystalValue = Math.floor(Math.random() * 11) + 1;
-//       //   	$("#purple").text(crystalValue);
-//       //   });
-// console.log(crystalValue);
+ 
     });
 
 

ea5563322e961d97b3b55c0d344c8609c1ed6609
Claire Pate <clairepate@Claires-Air.attlocal.net>
2017-10-16 20:42:41 -0400

    updated jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-var targetNum ="";
+var targetNum =50;
 var crystalValue="";
 var score = 0;
 var wins = 0;
@@ -7,21 +7,82 @@ var losses = 0;
 
 $(document).ready(function(){
 
+ $(".btn-success").on("click", function() {
+        var crystalValue = Math.floor(Math.random() * 11) + 1;
+        	$("#pink, #blue, #yellow, #purple").text(crystalValue);
+        	console.log(crystalValue);
 
+
+ 
 	$("#pink").on("click", function(){
-	alert("you clicked the pink crystal!")
+		score += crystalValue;
+		alert("New Score" + score);
+		if (score === targetNum){
+			alert("You Win!");
+		} else if (score >= targetNum)
+			alert("You Lose!");
+		console.log(score);
+
 	});
 
 	$("#blue").on("click", function(){
-	alert("you clicked the blue crystal!")
+		score += crystalValue;
+		alert("New Score" + score);
+		if (score === targetNum){
+			alert("You Win!");
+		} else if (score >= targetNum)
+			alert("You Lose!");
+		console.log(score);
 	});
 
 	$("#yellow").on("click", function(){
-	alert("you clicked the yellow crystal!")
+		score += crystalValue;
+		alert("New Score" + score);
+		if (score === targetNum){
+			alert("You Win!");
+		} else if (score >= targetNum)
+			alert("You Lose!");
+		console.log(score);
 	});
 
 	$("#purple").on("click", function(){
-	alert("you clicked the purple crystal!")
+		score += crystalValue;
+		alert("New Score" + score);
+		if (score === targetNum){
+			alert("You Win!");
+		} else if (score >= targetNum)
+			alert("You Lose!");
+		console.log(score);
 	});
 
+	$("#targetScore").text(targetNum);
+
+	$("#userScore").text(score);
+
+
+
+ // When random-button is clicked...
+//       $(".btn-success").on("click", function() {
+//         var crystalValue = Math.floor(Math.random() * 11) + 1;
+//         	$("#pink", "#blue", "#yellow", "#purple").text(crystalValue);
+
+//       // $(".btn-success").on("click", function() {
+//       //   var crystalValue = Math.floor(Math.random() * 11) + 1;
+//       //   	$("#blue").text(crystalValue);
+//       //   });
+
+//       // $(".btn-success").on("click", function() {
+//       //   var crystalValue = Math.floor(Math.random() * 11) + 1;
+//       //   	$("#yellow").text(crystalValue);
+//       //   });
+
+//       //  $(".btn-success").on("click", function() {
+//       //   var crystalValue = Math.floor(Math.random() * 11) + 1;
+//       //   	$("#purple").text(crystalValue);
+//       //   });
+// console.log(crystalValue);
+    });
+
+
 });
+

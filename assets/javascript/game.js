e756bc465a43428b4d022ea7a6b48e9469dd4fe9
Claire Pate <clairepate@Claires-MacBook-Air.local>
2017-10-18 14:46:15 -0400

    updated scoring and images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,69 +14,94 @@ $(document).ready(function(){
 
 // Functions to randomize crystal value and target number
  $(".btn-success").on("click", function() {
+
         var pinkValue = Math.floor(Math.random() * 11) + 1;
-        	$("#pink").text(pinkValue);
-        	console.log(pinkValue);;
+        	console.log("pink " + pinkValue);;
 
         var blueValue = Math.floor(Math.random() * 11) + 1;
-        	$("#blue").text(blueValue);
-        	console.log(blueValue);
+        	console.log("blue " + blueValue);
 
         var yellowValue = Math.floor(Math.random() * 11) + 1;
-        	$("#yellow").text(yellowValue);
-        	console.log(yellowValue);
+        	console.log("yellow " + yellowValue);
 
         var purpleValue = Math.floor(Math.random() * 11) + 1;
-        	$("#purple").text(purpleValue);
-        	console.log(purpleValue);
+        	console.log("purple " + purpleValue);
 
         var targetNum = Math.floor(Math.random() * 101) + 19;
         	$("#targetScore").text(targetNum);
         	console.log("target score" + targetNum);
 
+        // updating the wins
+    	var updatewins = function(){
+			document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		};
 
+		// updating the losses
+		var updatelosses = function(){
+			document.querySelector("#losses").innerHTML = "Losses: " + losses;
+		};
 
+var reset = function (){
+	var targetNum = "";
+	var pinkValue="";
+	var blueValue="";
+	var yellowValue="";
+	var purpleValue="";
+	var score = 0;
+	console.log(reset);
+};
 
  // Functions to increase score/ wins/ losses when the crystal is selected
 	$("#pink").on("click", function(){
 		$("#userScore").text(score += pinkValue);
 		if (score === targetNum){
-			alert("You Win!");
-		} else if (score >= targetNum) 
-			alert("You Lose!");
+			wins++;
+			updatewins();
+			reset();
+			
+		} if (score > targetNum) 
 			losses++;
-		console.log("score" + score);
-
+			updatelosses();
+			reset();
 	});
 
 	$("#blue").on("click", function(){
 		$("#userScore").text(score += blueValue);
 		if (score === targetNum){
-			alert("You Win!");
-		} else if (score >= targetNum)
-			alert("You Lose!");
+			wins++;
+			updatewins();
+			reset();
+			
+		} if (score > targetNum) 
 			losses++;
-		console.log(score);
+			updatelosses();
+			reset();
 	});
 
 	$("#yellow").on("click", function(){
 		$("#userScore").text(score += yellowValue);
 		if (score === targetNum){
-			alert("You Win!");
-		} else if (score >= targetNum)
-			alert("You Lose!");
+			wins++;
+			updatewins();
+			reset();
+			
+		} if (score > targetNum) 
 			losses++;
-		console.log(score);
+			updatelosses();
+			reset();
 	});
 
 	$("#purple").on("click", function(){
 		$("#userScore").text(score += purpleValue);
 		if (score === targetNum){
-			alert("You Win!");
-		} else if (score >= targetNum)
-			alert("You Lose!");
+			wins++;
+			updatewins();
+			reset();
+			
+		} if (score > targetNum) 
 			losses++;
-		console.log(score);
+			updatelosses();
+			reset();
 	});
 
 
